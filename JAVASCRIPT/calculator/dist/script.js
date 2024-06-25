08064886690a23a03dae5626350a38f7d202fa44
1flandrav <156852409+1flandrav@users.noreply.github.com>
2024-06-25 12:49:18 -0700

    hi

---
+++ JAVASCRIPT/calculator/dist/script.js
@@ -0,0 +1,26 @@
+const calcOperation= document.querySelector('.calc-operation');
+const calcTyped =document.querySelector('.calc-typed');
+function youClicked(n){
+if(n === '='){
+    if(calcOperation.textContent!=''){
+        calcTyped.textContent=eval(calcOperation.textContent);
+    }
+}
+else{
+    calcOperation.textContent += n;
+    
+}
+
+
+if(n === 'C'){
+    calcOperation.textContent="";
+    calcTyped.textContent="0";
+}
+
+if(n === 'openC'){
+calcOperation.textContent +='(';
+}
+if(n=== 'closeC'){
+    calcOperation.textContent+=')';
+}
+}
